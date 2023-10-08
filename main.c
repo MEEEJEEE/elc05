8f94ff9059e8fa1bc51f32d19701e3ed75b0fcc4
miji <mijilee0413@sookmyung.ac.kr>
2023-10-08 14:03:43 +0900

    5-1
    <양수/음수 판단>

--- main.c
+++ main.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	int input;
-	
-	printf("Input an integer : ");
-	
-	//정수가 아닌 값을 입력받았을때 에러 표시
-	if (scanf("%d", &input) != 1) {
-        printf("Invalid input. Please enter an integer.\n");
-        return 1;
-    }
-    
-	if (input < 0)
-	    input = -input;
+int main() {
+    int num;
 
-	    
-	printf("Absolute value is %d\n", input);
-	
-	return 0;    
-	
+    // 1개의 정수 입력
+    printf("Input an integer: ");
+    scanf("%d", &num);
+
+    // 입력된 숫자가 양수, 음수, 0인지를 판단하여 출력
+    if (num > 0) {
+        printf("The number is positive.\n");
+    } else if (num < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
 }
